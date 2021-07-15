593e93fc0f7d133f7d3eeb7e14043e5beca467f2
Poojan Patel <ppoojan40@gmail.com>
2021-07-15 14:55:41 +0530

    Chat-App

---
+++ ChatProject/ChatApp/urls.py
@@ -0,0 +1,30 @@
+"""ChatProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ChatApp import views
+
+urlpatterns = [
+    path('', views.home_fun, name='home'),
+    path('login/', views.login_fun, name='login-page'),
+    path('<str:room>/', views.room, name='room'),
+    # path('room-check/', views.room_check_fun, name='room-check-page'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+    # path('admin/', admin.site.urls),
+    # path('home/', include('ChatApp.urls'))
+]
